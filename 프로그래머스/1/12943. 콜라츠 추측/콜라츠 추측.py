a5dce0ff5f2fcc16319c53cb9b65913de4fc9d25
Soboro <150016105+So8oro@users.noreply.github.com>
2026-04-25 05:13:51 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.22 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,9 @@
+def solution(num):
+    if num == 1: return 0
+    for cnt in range(1,501):
+        if num%2 == 0:
+            num /= 2
+        else:
+            num = num*3 + 1
+        if num == 1: return cnt
+    return -1
