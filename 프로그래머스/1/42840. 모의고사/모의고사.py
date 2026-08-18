0cebbb167331d49cab63f399ffe1efb349e420b7
Soboro <150016105+So8oro@users.noreply.github.com>
2026-08-19 01:20:30 +0900

    [level 1] Title: 모의고사, Time: 1.15 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,17 @@
+def solution(answers):
+    answer = []
+    score = [0,0,0]
+    length = len(answers)
+    one = [1,2,3,4,5] * (len(answers)//5 + 1)
+    two = [2,1,2,3,2,4,2,5] * (len(answers)//8 + 1)
+    thr = [3,3,1,1,2,2,4,4,5,5] * (len(answers)//10 + 1)
+    
+    for i in range(length):
+        if answers[i] == one[i]: score[0] += 1
+        if answers[i] == two[i]: score[1] += 1
+        if answers[i] == thr[i]: score[2] += 1
+    
+    for i in range(3):
+        if score[i] == max(score): answer.append(i+1)
+    
+    return answer
